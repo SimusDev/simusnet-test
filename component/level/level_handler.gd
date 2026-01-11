4e3b6bdd2cc62b3a587b4e89ce4a27866c5cfdbc
zxcvlad78 <danyaps@mail.ru>
2026-01-11 23:33:14 +0300

    sewx

--- component/level/level_handler.gd
+++ component/level/level_handler.gd
@@ -1,5 +1,7 @@
 class_name LevelHandler extends Node
 
+signal level_changed
+
 @export var base_path:String = "res://"
 @export var directories:PackedStringArray
 @export var level_at_start:R_Level
@@ -8,7 +10,36 @@ class_name LevelHandler extends Node
 var current_level:R_Level
 
 func _ready() -> void:
+	SimusNetRPC.register(
+		[
+			_send,
+		],
+		SimusNetRPCConfig.new().flag_mode_any_peer()
+	)
+	SimusNetRPC.register(
+		[_receive],
+		SimusNetRPCConfig.new().flag_mode_server_only()
+	)
 	_handle()
+	
+	if level_at_start and SimusNetConnection.is_server():
+		change_level(level_at_start)
+	
+	if not SimusNetConnection.is_server():
+		SimusNetRPC.invoke_on_server(_send)
+	
+	
+
+func _send() -> void:
+	SimusNetRPC.invoke_on(
+		SimusNetRemote.sender_id,
+		_receive,
+		current_level
+	)
+
+func _receive(_current_level:R_Level) -> void:
+	current_level = _current_level
+	change_level(current_level)
 
 func _handle() -> void:
 	for directory:String in directories:
@@ -16,9 +47,7 @@ func _handle() -> void:
 			var resource:Resource = load(file)
 			if resource is R_Level:
 				resource.register()
-	
-	if level_at_start:
-		change_level(level_at_start)
+
 
 func clear(safe:bool = true) -> void:
 	if safe:
@@ -30,7 +59,10 @@ func change_level(to:R_Level) -> void:
 	if not is_instance_valid(level_holder):
 		return
 	clear(false)
-	to.instantiate(level_holder)
+	current_level = to
+	if to:
+		to.instantiate(level_holder)
+	level_changed.emit()
 
 func change_level_by_code(level_code:StringName) -> void:
 	for ref in R_Level.get_reference_list():
