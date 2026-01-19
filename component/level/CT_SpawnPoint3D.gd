48135e4cdb31173b8f59513e7e24dc40bd72696e
zxcvlad78 <danyaps@mail.ru>
2026-01-19 15:01:24 +0300

    GOMIIIKK

--- component/level/CT_SpawnPoint3D.gd
+++ component/level/CT_SpawnPoint3D.gd
@@ -1,5 +1,15 @@
+@tool
 extends Node3D
 class_name CT_SpawnPoint3D
+
+var view_mesh:Mesh :
+	set(val):
+		view_mesh = val
+		
+		if is_instance_valid(view_mesh_instance):
+			view_mesh_instance.mesh = view_mesh
+
+var view_mesh_instance:MeshInstance3D
 
 static var _list: Array[CT_SpawnPoint3D] = []
 
@@ -11,3 +21,19 @@ func _enter_tree() -> void:
 
 func _exit_tree() -> void:
 	_list.erase(self)
+
+func _ready() -> void:
+	if not Engine.is_editor_hint():
+		return
+	if not view_mesh:
+		view_mesh = load("res://src/meshes/sp_box_mesh.tres")
+	
+	if view_mesh_instance:
+		if is_instance_valid(view_mesh_instance):
+			remove_child(view_mesh_instance)
+			view_mesh_instance.queue_free()
+		view_mesh_instance = null
+	
+	view_mesh_instance = MeshInstance3D.new()
+	view_mesh_instance.mesh = view_mesh
+	add_child(view_mesh_instance)
