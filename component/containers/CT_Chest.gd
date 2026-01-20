e72d6b6961199d0a6dc150b6e905705706f925d2
SimusDev <simusdev@mail.ru>
2026-01-20 07:50:10 +0300

    chest component!

---
+++ component/containers/CT_Chest.gd
@@ -0,0 +1,27 @@
+extends Node
+class_name CT_Chest
+
+@export var root: Node3D
+@export var inventory: CT_Inventory
+
+const OPEN_ACTION: R_InteractAction = preload("uid://xecviu4tcm2e")
+const BACKPACK_INTERFACE: PackedScene = preload("uid://pa0ttv4j400k")
+
+func _ready() -> void:
+	if !root:
+		root = get_parent()
+	
+	if !root.is_node_ready():
+		await root.ready
+	
+	if !inventory:
+		inventory = CT_Inventory.new()
+		inventory.node = root
+		root.add_child(inventory)
+	
+	if root and inventory:
+		if root is CollisionObject3D:
+			CT_Collisions.set_body_collision(root, CT_Collisions.LAYERS.INTERACTION, true, true)
+		
+		OPEN_ACTION.append_to(root)
+		inventory.backpack_interface = BACKPACK_INTERFACE
