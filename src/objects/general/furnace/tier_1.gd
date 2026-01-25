9441f52470fa44387507498be9d15bf9cde66620
zxcvlad78 <danyaps@mail.ru>
2026-01-25 21:13:18 +0300

    sex

--- src/objects/general/furnace/tier_1.gd
+++ src/objects/general/furnace/tier_1.gd
@@ -1,8 +1,9 @@
 extends RigidBody3D
 
 func _ready() -> void:
+	_update()
+	$CT_Furnace.active_change.connect(_update)
+
+func _update() -> void:
 	$CPUParticles3D.emitting = $CT_Furnace.is_active()
-	$CT_Furnace.active_change.connect(
-		func():
-			$CPUParticles3D.emitting = $CT_Furnace.is_active()
-			)
+	$OmniLight3D.visible = $CT_Furnace.is_active()
