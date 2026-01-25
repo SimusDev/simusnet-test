0454b639ed53793d00985ac225e0b8163dc6a20a
SimusDev <simusdev@mail.ru>
2026-01-25 23:16:02 +0300

    gamik

--- addons/gevp/scripts/vehicle_synchronizer.gd
+++ addons/gevp/scripts/vehicle_synchronizer.gd
@@ -5,6 +5,12 @@ class_name VehicleSynchronizer
 @export var camera: Camera3D
 
 func _ready() -> void:
+	SimusNetVars.register(vehicle, [
+		"speed", "local_velocity", "throttle_input",
+		"motor_rpm", "linear_velocity"
+	]
+	, SimusNetVarConfig.new().flag_replication())
+	
 	if SimusNet.is_network_authority(self):
 		await SD_Nodes.async_for_ready(vehicle)
 		camera.make_current()
