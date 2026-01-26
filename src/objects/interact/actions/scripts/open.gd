d80dd7e612221f7ece45c1370b252374d1ad31b9
SimusDev <simusdev@mail.ru>
2026-01-26 03:13:04 +0300

    движок говна выделенный сервер не работает!

--- src/objects/interact/actions/scripts/open.gd
+++ src/objects/interact/actions/scripts/open.gd
@@ -1,7 +1,6 @@
 extends R_InteractAction
 
 func _server_selected_world(object: Node3D, raycast: CT_InteractionRay) -> void:
-	
 	var inventory: CT_Inventory = CT_Inventory.find_in(raycast.root)
 	var object_inventory: CT_Inventory = CT_Inventory.find_in(object)
 	
