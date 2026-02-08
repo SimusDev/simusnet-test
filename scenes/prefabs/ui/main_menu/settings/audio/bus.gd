e6f11ebb6bb5d887649bcd35ab90e9f2de2db5d7
SimusDev <simusdev@mail.ru>
2026-02-09 00:50:25 +0300

    keybindings and audio settings WIP

---
+++ scenes/prefabs/ui/main_menu/settings/audio/bus.gd
@@ -0,0 +1,18 @@
+extends Panel
+
+var bus: SD_AudioBus
+
+@onready var name_label: Label = $Name
+@onready var h_slider: HSlider = $HSlider
+
+func _ready() -> void:
+	_update_percents()
+	h_slider.value = bus.get_volume()
+
+func _update_percents() -> void:
+	name_label.text = bus.get_name() + " - " + str(round(bus.get_volume() * 100.0)) + "%"
+
+func _on_h_slider_drag_ended(value_changed: bool) -> void:
+	if value_changed:
+		bus.set_volume(h_slider.value)
+		_update_percents()
--- scenes/prefabs/ui/main_menu/settings/tab_audio.gd
+++ scenes/prefabs/ui/main_menu/settings/tab_audio.gd
@@ -1,1 +1,10 @@
 extends Control
+
+@export var _container: Control
+@export var _scene: PackedScene
+
+func _ready() -> void:
+	for bus in SimusDev.audio.get_bus_list():
+		var ui: Control = _scene.instantiate()
+		ui.bus = bus
+		_container.add_child(ui)
