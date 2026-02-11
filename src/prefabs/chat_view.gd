5ea2ae9ae058553a842ee37803dace71c480886c
zxcvlad78 <danyaps@mail.ru>
2026-02-11 21:04:01 +0300

    sex +++++++++++ chAT ____)))) SEX INTERNET

---
+++ src/prefabs/chat_view.gd
@@ -0,0 +1,48 @@
+extends Panel
+
+@export var chat: SimusNetChat
+@export var text_label: RichTextLabel
+@export var hide_delay:float = 5.0
+@export var hide_duration:float = 1.0
+
+var tween:Tween
+
+func _ready() -> void:
+	if not is_multiplayer_authority():
+		process_mode = Node.PROCESS_MODE_DISABLED
+		return
+	if not chat:
+		return
+	
+	tween = create_tween()
+	
+	chat.on_message_received.connect(_on_message_received)
+	s_Users.on_connected.connect(_on_user_connected)
+
+func _on_user_connected(user:CT_User) -> void:
+	_add_message("[color=orange]%s connected[/color]" % [user.get_nickname()])
+
+func _on_message_received(msg: SimusNetChatMessage) -> void:
+	if not text_label:
+		return
+	
+	_add_message(_get_message_text(msg))
+
+func _add_message(text:String):
+	text_label.text += text + "\n"
+	
+	if tween:
+		tween.kill() 
+	
+	modulate.a = 1.0
+	
+	tween = create_tween()
+	
+	tween.tween_interval(hide_delay)
+	tween.tween_property(self, "modulate:a", 0.0, hide_duration)
+
+func _get_message_text(msg: SimusNetChatMessage) -> String:
+	return "%s: %s\n" % [
+		CT_User.find_by_peer(msg.get_peer_id()),
+		msg.get_text()
+		]
--- src/prefabs/game_commands/game_commands.gd
+++ src/prefabs/game_commands/game_commands.gd
@@ -7,16 +7,21 @@ func _ready() -> void:
 func _on_command_executed(command:SD_ConsoleCommand) -> void:
 	match command.get_code():
 		"player.kill":
-			if command.get_arguments().size() < 1:
-				return
-			var user = CT_User.server_find_by_login(command.get_value_as_string())
-			if not user:
-				return
-			
-			var health:CT_Health = SD_ECS.find_first_component_by_script(user.get_player_node(), [CT_Health]) as CT_Health
-			if health:
-				health.kill()
-			else:
-				user.get_player_node().queue_free()
+			SimusNetRPC.invoke_on_server(
+				_player_kill,
+				command.get_value_as_string()
+			)
 			
 			return
+
+func _player_kill(login:String) -> void:
+	var user = CT_User.server_find_by_login(login)
+	if not user:
+		return
+	
+	var health:CT_Health = SD_ECS.find_first_component_by_script(user.get_player_node(), [CT_Health]) as CT_Health
+	if health:
+		health.kill()
+	else:
+		user.get_player_node().queue_free()
+	pass
