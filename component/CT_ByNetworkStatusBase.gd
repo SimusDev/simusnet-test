a2fed574c3b502489a8834df56b991b3bcc7c4c3
SimusDev <simusdev@mail.ru>
2026-02-17 08:03:59 +0300

    Update CT_ByNetworkStatusBase.gd

--- component/CT_ByNetworkStatusBase.gd
+++ component/CT_ByNetworkStatusBase.gd
@@ -4,30 +4,25 @@ class_name CT_ByNetworkStatusBase
 
 enum STATUS {
 	SERVER,
-	NOT_SERVER,
 	DEDICATED_SERVER,
 	CLIENT,
 }
 
 @export var status: STATUS = STATUS.SERVER
+@export var condition: bool = true
 
 func _network_ready() -> void:
 	match status:
 		STATUS.DEDICATED_SERVER:
-			if SimusNetConnection.is_dedicated_server():
+			if SimusNetConnection.is_dedicated_server() == condition:
 				_status_success()
 				return
 		STATUS.SERVER:
-			if SimusNetConnection.is_server():
+			if SimusNetConnection.is_server() == condition:
 				_status_success()
 				return
-		STATUS.NOT_SERVER:
-			if !SimusNetConnection.is_server():
-				_status_success()
-				return
-			
 		STATUS.CLIENT:
-			if SimusNetConnection.is_client():
+			if SimusNetConnection.is_client() == condition:
 				_status_success()
 				return
 	
